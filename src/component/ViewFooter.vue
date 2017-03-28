c04925e4f211971deba9c86bc2cc4a64388dc6ec
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2017-03-28 11:46:56 -0300

    Add footer year

--- src/component/ViewFooter.vue
+++ src/component/ViewFooter.vue
@@ -1,9 +1,24 @@
 <template lang="pug">
   footer.view-footer
-    p
+    p.copyright
+      a.link(href="https://vitorluizc.github.io/") Vitor Cavalcanti
+      |  &copy; {{ year }}
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        year: new Date().getFullYear()
+      }
+    }
+  }
+</script>
 
 <style lang="stylus">
   .view-footer
     color: #000
+
+    & > .copyright
+      text-align: center
 </style>
