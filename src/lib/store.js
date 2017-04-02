d35a1b2a9ca82f39d891d9314bf54f4773b2f5e7
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2017-04-02 18:10:44 -0300

    Add async/await support

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,10 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { authentication, database, storage } from './application.js'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-
+  state: {
+    user: authentication.currentUser || null,
+    ideas: [],
+    tags: []
+  },
+  getters: {
+    userProfile(state) {
+      return (!!state.user) ? 'user' : 'guest'
+    },
+    user(state) {
+      return state.user
+    }
+  },
+  mutations: {
+    'add-user'(store, user) {
+      store.user = user
+    }
+  },
+  actions: {
+    async signIn({ commit }, email, password) {
+      let user = await authentication.signInWithEmailAndPassword(email, password)
+      commit('add-user', user)
+      console.log(user)
+    }
+  }
 })
 
 export default store
