8025e3d7e1ed0a7129fa7ac95dc1772b31da1ae5
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2017-04-02 14:35:16 -0300

    Fix UglifyJS plugin + Add Vuex

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import './view.pug'
+import './index.pug'
 import Vue from 'vue'
+import Main from './components/Main.vue'
+import store from './lib/store.js'
 import router from './lib/router.js'
-import Main from './component/Main.vue'
 
 new Vue({
+  store,
   router,
   el: '#application',
   render: createElement => createElement(Main)
--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,9 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const { extractStyle, rules } = require('./webpack.rules.js')
 const { DefinePlugin, LoaderOptionsPlugin } = webpack
 const { UglifyJsPlugin } = webpack.optimize;
-
-const vue = {
-  test: /\.vue$/,
-  exclude: /node_modules/,
-  use: {
-    loader: 'vue-loader',
-    options: {
-      loaders: {
-        stylus: ExtractTextPlugin.extract({
-          use: ['css-loader', 'stylus-loader'],
-          fallback: 'vue-style-loader'
-        })
-      }
-    }
-  }
-}
-
-const babel = {
-  test: /\.js$/,
-  exclude: /node_modules/,
-  use: {
-    loader: 'babel-loader',
-    options: {
-      presets: [
-        ['es2015', { modules: false }]
-      ]
-    }
-  }
-}
-
-const pug = {
-  test: /\.pug$/,
-  use: {
-    loader: 'pug-loader',
-    options: {
-      pretty: true
-    }
-  }
-}
-
-const image = {
-  test: /\.(png|jpe?g|svg)$/,
-  use: [
-    {
-      loader: 'file-loader',
-      options: {
-        name: '/img/[name].[ext]?[hash]'
-      }
-    },
-    {
-      loader: 'image-webpack-loader',
-      options: {}
-    }
-  ]
-}
-
-const font = {
-  test: /\.woff2?$/,
-  exclude: /node_modules/,
-  use: [
-    {
-      loader: 'file-loader',
-      options: {
-        name: '/font/[name].[ext]'
-      }
-    }
-  ]
-}
 
 const config = {
   entry: './src/index.js',
@@ -79,27 +11,16 @@ const config = {
     filename: 'js/[name].js',
     path: path.join(__dirname, './dist')
   },
-  module: {
-    rules: [ vue, babel, pug, image, font ]
-  },
-  resolve: {
-    alias: {
-      vue$: 'vue/dist/vue.common.js'
-    }
-  },
-  devServer: {
-    compress: true,
-    port: 9000
-  },
+  module: { rules },
   plugins: [
-    new ExtractTextPlugin('./css/style.css'),
+    extractStyle,
     new HtmlPlugin({
-      template: 'src/view.pug',
+      template: 'src/index.pug',
       filename: 'index.html'
     })
   ],
   performance: { hints: false },
-  devtool: '#source-map'
+  devtool: 'source-map'
 }
 
 /**
@@ -107,16 +28,15 @@ const config = {
  * @returns {Webpack.Configuration}
  */
 module.exports = env => {
-  if (env === 'production') {
+  const isProd = (env === 'production')
+
+  if (isProd) {
     config.devtool = false
     config.plugins = (config.plugins || []).concat([
       new DefinePlugin({
         'process.env': {
           NODE_ENV: '"production"'
         }
-      }),
-      new LoaderOptionsPlugin({
-        minimize: true
       }),
       new UglifyJsPlugin({
         sourceMap: false,
