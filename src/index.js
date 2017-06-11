8c9f5d0858e4886de7650bad2aeb10d30947d496
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2017-06-11 19:06:16 -0300

    Fix importing store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.pug'
 import Vue from 'vue'
 import Main from './components/Main.vue'
-import store from './lib/store.js'
+import store from './store'
 import router from './lib/router.js'
 
 new Vue({
--- src/lib/router.js
+++ src/lib/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import store from './store.js'
+import store from '../store'
 import { ViewLogin, ViewSubscribe, ViewDashboard } from '../components/views'
 
 Vue.use(Router)
